9809e11dcc6e116dc7233aaa957545c4a0f887e8
Arkadiusz Adamski <ar4s@users.noreply.github.com>
2021-11-19 16:22:15 +0100

    Fix include (#10)

--- install-linux-dependencies.sh
+++ install-linux-dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 MOZJPEG_VER=3.3.1
 
@@ -6,19 +7,17 @@ mkdir -p /app && cd /app
 
 mkdir -p /build/lib && mkdir -p /build/bin
 
-ADD . .
-
-apt-get update 
-apt-get install -y wget libssl-dev autoconf automake libtool pkg-config nasm make 
+apt-get update
+apt-get install -y wget libssl-dev autoconf automake libtool pkg-config nasm make
 
 # build and install mozjpeg
-wget https://github.com/mozilla/mozjpeg/archive/v$MOZJPEG_VER.tar.gz 
-tar -xvf v$MOZJPEG_VER.tar.gz 
-cd mozjpeg-$MOZJPEG_VER  
-autoreconf -fiv 
-mkdir build 
-cd build 
-sh ../configure && make install 
-rm /app/v$MOZJPEG_VER.tar.gz 
-echo "/opt/mozjpeg/lib64/" >> /etc/ld.so.conf.d/mozjpeg.conf 
+wget https://github.com/mozilla/mozjpeg/archive/v$MOZJPEG_VER.tar.gz
+tar -xvf v$MOZJPEG_VER.tar.gz
+cd mozjpeg-$MOZJPEG_VER
+autoreconf -fiv
+mkdir build
+cd build
+sh ../configure && make install
+rm /app/v$MOZJPEG_VER.tar.gz
+echo "/opt/mozjpeg/lib64/" >> /etc/ld.so.conf.d/mozjpeg.conf
 ldconfig
